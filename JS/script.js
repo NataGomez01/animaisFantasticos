941fbf200715da661a86cc765a93049622490874
NataGomez01 <nata.gomes@etec.sp.gov.br>
2020-11-25 23:54:07 -0300

    First Commit

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+import initScrollSuave from './modules/scoll-suave.js'
+import initScrollAnimation from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdown from './modules/dropdown.js'
+import initMenuMobile from './modules/menu-mobile.js'
+import initFetchBitcoin from './modules/fetch-bitcoin.js'
+import initFuncionamento from './modules/funcionamento.js'
+import initFetchAnimais from './modules/fetch-animais.js'
+
+initScrollSuave()
+initScrollAnimation()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
+initDropdown()
+initMenuMobile()
+initFuncionamento()
+initFetchAnimais()
+initFetchBitcoin()
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["@babel/polyfill", "whatwg-fetch", "./JS/script.js"],
+  output: {
+    path: path.resolve(__dirname, "./"),
+    filename: "./main.js/main.js",
+  },
+};
