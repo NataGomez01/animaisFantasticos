82232cf4aa0959616c3b96e620afc6e9227ea377
NataGomez01 <nata.gomes@etec.sp.gov.br>
2020-11-26 16:01:36 -0300

    scroll suave tranformado em classe

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: ["@babel/polyfill", "whatwg-fetch", "./JS/script.js"],
   output: {
     path: path.resolve(__dirname, "./"),
-    filename: "./main.js/main.js",
+    filename: "main.js/main.js",
   },
 };
